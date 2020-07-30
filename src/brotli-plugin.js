3103020b5df1bce6c6ddfcfc907c4fe2196b0593
Osmond van Hemert <osmond@vanhemert.nu>
2020-07-30 16:41:29 +0200

    add main plugin tests

--- src/brotli-plugin.js
+++ src/brotli-plugin.js
@@ -7,6 +7,7 @@ const util = require('util')
 const Piscina = require('piscina')
 
 const defaultOptions = {
+  cwd: path.join(process.cwd(), 'public'),
   extensions: ['css', 'js'],
   path: ''
 }
@@ -17,14 +18,19 @@ async function onPostBuild ({ reporter }, pluginOptions) {
   const options = { ...defaultOptions, ...pluginOptions }
 
   // get the files
-  const fileBasePath = path.join(process.cwd(), 'public')
   const patternExt = (options.extensions.length > 1) ? `{${options.extensions.join(',')}}` : options.extensions[0]
   const pattern = `**/*.${patternExt}`
-  const files = await globAsync(pattern, { cwd: fileBasePath, ignore: '**/*.br', nodir: true })
+  const globResult = await globAsync(pattern, { cwd: options.cwd, ignore: '**/*.br', nodir: true })
+  const files = globResult.map(res => {
+    return {
+      from: path.join(options.cwd, res),
+      to: path.join(options.cwd, options.path, `${res}.br`)
+    }
+  })
 
   // compress using worker pool
   const pool = new Piscina({ filename: path.resolve(__dirname, 'worker.js') })
-  const compress = files.map(file => pool.runTask({ file, options }))
+  const compress = files.map(file => pool.runTask(file))
   await Promise.all(compress)
 
   reporter.info(`Brotli compressed ${pool.completed} files - ${(pool.duration / 1000).toFixed(3)}s - ${(pool.runTime.average / 1000).toFixed(3)}/s`)
