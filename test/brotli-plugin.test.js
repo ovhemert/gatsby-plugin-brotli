a54d94dc04273c592cad00203568f01aa5e13d43
Osmond van Hemert <osmond@vanhemert.nu>
2020-07-30 20:38:03 +0200

    fix cwd in tests

--- test/brotli-plugin.test.js
+++ test/brotli-plugin.test.js
@@ -4,11 +4,9 @@ const sinon = require('sinon')
 const test = require('tap').test
 
 const TEST_DIR = {
-  public: {
-    'test.css': 'contents of a .css file',
-    'test.html': 'contents of a .html file',
-    'test.js': 'contents of a .js file'
-  }
+  'test.css': 'contents of a .css file',
+  'test.html': 'contents of a .html file',
+  'test.js': 'contents of a .js file'
 }
 
 const tested = require('../src/brotli-plugin')
@@ -18,9 +16,9 @@ test('compress only default extensions', t => {
   const args = { reporter: { info: sinon.fake() } }
   tested.onPostBuild(args, { cwd })
     .then(res => {
-      t.ok(fs.existsSync(path.join(cwd, 'public/test.css.br')))
-      t.ok(fs.existsSync(path.join(cwd, 'public/test.js.br')))
-      t.notOk(fs.existsSync(path.join(cwd, 'public/test.html.br')))
+      t.ok(fs.existsSync(path.join(cwd, 'test.css.br')))
+      t.ok(fs.existsSync(path.join(cwd, 'test.js.br')))
+      t.notOk(fs.existsSync(path.join(cwd, 'test.html.br')))
     })
     .catch(err => t.fail(err))
     .finally(() => t.end())
@@ -31,9 +29,21 @@ test('compress only single extension', t => {
   const args = { reporter: { info: sinon.fake() } }
   tested.onPostBuild(args, { cwd, extensions: ['css'] })
     .then(res => {
-      t.ok(fs.existsSync(path.join(cwd, 'public/test.css.br')))
-      t.notOk(fs.existsSync(path.join(cwd, 'public/test.js.br')))
-      t.notOk(fs.existsSync(path.join(cwd, 'public/test.html.br')))
+      t.ok(fs.existsSync(path.join(cwd, 'test.css.br')))
+      t.notOk(fs.existsSync(path.join(cwd, 'test.js.br')))
+      t.notOk(fs.existsSync(path.join(cwd, 'test.html.br')))
+    })
+    .catch(err => t.fail(err))
+    .finally(() => t.end())
+})
+
+test('compress to another folder', t => {
+  const cwd = t.testdir(TEST_DIR)
+  const dir = 'brotli'
+  const args = { reporter: { info: sinon.fake() } }
+  tested.onPostBuild(args, { cwd, path: dir })
+    .then(res => {
+      t.ok(fs.existsSync(path.join(cwd, dir, 'test.css.br')))
     })
     .catch(err => t.fail(err))
     .finally(() => t.end())
